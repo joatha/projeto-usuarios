5d9de3487304f994627cce431428d9b322ba5f5e
Joathã Andrade <74799613+joatha@users.noreply.github.com>
2020-11-30 15:11:58 -0300

    Atualizando

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var nome = document.querySelector("#exampleInputName");
+var genenrom = document.querySelector("#exampleInputGenderM");
+var generof = document.querySelector("#exampleInputGenderF");
+var ani = document.querySelector("#exampleInputBirth");
+var pais = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail1");
+var senha = document.querySelector("#exampleInputPassword1");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#");
+
