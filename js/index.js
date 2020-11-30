57dc77bc1a600e5a2e94c706e21bafb99b104ea9
Joathã Andrade <74799613+joatha@users.noreply.github.com>
2020-11-30 16:11:48 -0300

    Atualizando

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
 var nome = document.querySelector("#exampleInputName");
-var genenrom = document.querySelector("#exampleInputGenderM");
-var generof = document.querySelector("#exampleInputGenderF");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 var ani = document.querySelector("#exampleInputBirth");
 var pais = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail1");
 var senha = document.querySelector("#exampleInputPassword1");
 var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#");
+var admin = document.querySelector("exampleInputAdmin");
 
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+
+    if(field.name =="gender"){
+
+        if(field.checked){
+            console.log("SIM", field);
+
+        }
+   
+    } else{
+        console.log("NÃO");
+    }
+
+    //console.log(index, field.value, field.id);
+
+});
