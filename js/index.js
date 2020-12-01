9348d3434272eff16fae3044b8b938ff08a19313
Joathã Andrade <74799613+joatha@users.noreply.github.com>
2020-12-01 19:38:53 -0300

    Atualizando

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ var user = {};
 
 function addLine(dataUser){
 
-    var tr = document.createElement("tr");
-    
-    tr.innerHTML = `
+    console.log(dataUser);
+
+       
+    document.getElementById("table-users").innerHTML = `
             <tr>
                     <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
                     <td>${dataUser.name}</td>
@@ -18,7 +19,7 @@ function addLine(dataUser){
                 </td>
             </tr>
     `;
-    document.getElementById("table-users").appendChild(tr);
+    
 
 }
 
@@ -40,5 +41,16 @@ document.getElementById("form-user-create").addEventListener("submit", function(
       
     });
     
+    var obrjetUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.coutry,
+        user.email,
+        user.password,
+        user.photo, 
+        user.admin
+    );
+
     addLine(user);
 });
