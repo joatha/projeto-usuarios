f1454cbf03cdcc2a68701d34175fbf76ba5b2bf7
Joathã Andrade <74799613+joatha@users.noreply.github.com>
2020-12-06 20:51:51 -0300

    Atualizando

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,9 +14,59 @@ class UserController{
 
             event.preventDefault();
 
-            this.addLine(this.getValues());
+            let values = this.getValues();
+            
+            this.getPhoto().then(
+                
+                (content)=>{
+
+                values.photo = content;
+
+                this.addLine(values);
+
+
+            }, 
+            
+            (e) => {
+
+                console.error(e);
+
+            });
+
+
+            
+        });
+    }
+
+    getPhoto(){
+
+        return new Promise(( resolve, reject)=>{
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item =>{
+            
+                 if (item.name === 'photo'){
+                    return item;
+            }
 
         });
+            let file = elements[0].files[0];
+
+            fileReader.onload = ()=>{
+    
+                
+                resolve(fileReader.result);
+    
+            };
+            fileReader.onerror = (e)=>{
+                reject(e);
+            }
+    
+            fileReader.readAsDataURL(file);
+        });
+                  
+       
     }
     
     getValues(){
@@ -56,7 +106,7 @@ class UserController{
                     
         this.tableEl.innerHTML = `
                 <tr>
-                        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
                         <td>${dataUser.email}</td>
                         <td>${dataUser.admin}</td>
